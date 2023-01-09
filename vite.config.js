578b6d39eee7f93c4e337f41b3faeb2204fe85d0
mr cod3r <mrcod3r.ir@gmail.com>
2023-01-09 20:57:59 +0330

    add partial html by vite-plugin-handlebars

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import htmlPurge from "vite-plugin-html-purgecss";
+import handlebars from "vite-plugin-handlebars";
 
 const path = require("path");
-
 export default {
   root: path.resolve(__dirname, "src"),
   resolve: {
@@ -9,7 +9,10 @@ export default {
       "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
     },
   },
-  plugins: [htmlPurge()],
+  plugins: [
+    htmlPurge(),
+    handlebars({ partialDirectory: path.resolve(__dirname, "src/partials") }),
+  ],
   server: {
     port: 8080,
     hot: true,
